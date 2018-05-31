0109f425b2d3d6ed08649a8295167ac835ccdd7e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-31 09:55:03 -0700

    created static paging component

---
+++ src/components/Paging.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Books extends Component {
+
+  render() {
+    // const { items } = this.props;
+
+    return (
+      <p>
+        I am a page!
+      </p>
+    );
+  }
+}
