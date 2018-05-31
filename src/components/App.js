9c7168721bacf039ff094b8228187e2503e9b8d5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-30 17:36:34 -0700

    working on static app

--- src/components/App.js
+++ src/components/App.js
@@ -2,4 +2,16 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
     
+    render() {
+      return (
+        <div>
+            <header>
+                <Search/>
+            </header>
+            <main>
+                <Articles/>
+            </main>
+        </div>
+      );
+  }
 }
