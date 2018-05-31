07b128553a87bdc58c60ba065957e8db4e24e193
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-31 10:39:54 -0700

    updated properties to match that of the google's book api

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -1,15 +1,17 @@
 const BASE_URL = 'https://www.googleapis.com/books/v1';
 const VOLUMES_URL = `${BASE_URL}/volumes?`;
-const SORT_QUERY = 'sortBy=title';
+const SORT_QUERY = 'orderBy=relevance';
 
 const throwJson = json => { throw json; };
 const get = url => fetch(url)
   .then(r => r.ok ? r.json() : r.json().then(throwJson));
 
-export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 20 }) {
+export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 10 }) {
+  const bookIndex = (page * 10) - 10;
   const search = `&q=${topic}&sources=${sources.join()}`;
-  const paging = `&page=${page}&pageSize=${pageSize}`;
+  const maxResults = `&maxResults=${pageSize}`;
   const sort = `&${SORT_QUERY}`;
+  const startIndex = `&startIndex=${bookIndex}`;
 
-  return get(`${VOLUMES_URL}${search}${paging}${sort}`);
+  return get(`${VOLUMES_URL}${search}${maxResults}${sort}${startIndex}`);
 }
