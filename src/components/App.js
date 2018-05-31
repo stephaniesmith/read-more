7d8cf793aaa2dba1cdce297c4a29dda2081e5e78
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-30 19:05:44 -0700

    setting up the search

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
+import { search } from '../services/booksApi'
 import Search from './Search';
 import Books from './Books';
 
 export default class App extends Component {
     
-  render() {
-    return (
-      <div>
-        <header>
-          <p>Hello World</p>
-          <Search/>
-        </header>
-        <main>
-          <Books/>
-        </main>
-      </div>
-    );
-  }
+    state = {
+      volume: '',
+      page: 1,
+      perPage: 20,
+      totalItems: 0,
+      items: []
+    };
+
+    searchBooks = () => {
+      const { volume, page, perPage } = this.state;
+
+      search({ volume }, { page, perPage })
+        .then(({ items, totalItems }) => {
+            this.setState({ items, totalItems });
+        });
+    };
+
+    render() {
+      return (
+        <div>
+          <header>
+            <p>Hello World</p>
+            <Search/>
+          </header>
+          <main>
+            <Books/>
+          </main>
+        </div>
+      );
+    }
 }
