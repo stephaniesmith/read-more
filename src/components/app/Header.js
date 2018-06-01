34cf882cc37f046f86234078dbf582e04a00d813
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-01 10:02:44 -0700

    static header setup

---
+++ src/components/app/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    
+  render() {
+
+    return (
+      <li>
+        <h1>Find a Book</h1>
+        <p>HEADER!</p>
+      </li>
+    );
+  }
+}
