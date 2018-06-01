9de157def75dc11cee2ec9d4b7d470898be5cf7a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-01 11:08:42 -0700

    working on setting up the router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { search } from '../../services/booksApi'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { search } from '../../services/booksApi';
 import Header from './Header';
-import Search from '../Search';
-import Books from '../Books';
-import Paging from '../Paging';
+import Home from './Home';
+import Search from '../search/Search';
+// import Books from '../Books';
+// import Paging from '../Paging';
 
 export default class App extends Component {
     
@@ -44,26 +46,30 @@ export default class App extends Component {
       const { topic, loading, error, page, perPage, totalItems, items } = this.state;
 
       return (
-        <div>
-          <header>
+        <Router>
+          <div>
             <Header/>
-            <Search onSearch={this.handleSearch}/>
-          </header>
-          <main>
-            <section>
-              {loading && <div>Loading...</div>}
-              {error && <div>Error: {error.message}</div>}
-              <p>Results for: {topic}</p>
-            </section>
-            <Paging
-              totalItems={totalItems}
-              page={page}
-              perPage={perPage}
-              onPage={this.handlePage}
-            />
-            <Books items={items}/>
-          </main>
-        </div>
+              {/* <Search onSearch={this.handleSearch}/> */}
+            <main>
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/search" component={Search}/>
+              </Switch>
+              {/* <section>
+                {loading && <div>Loading...</div>}
+                {error && <div>Error: {error.message}</div>}
+                <p>Results for: {topic}</p>
+              </section>
+              <Paging
+                totalItems={totalItems}
+                page={page}
+                perPage={perPage}
+                onPage={this.handlePage}
+              />
+              <Books items={items}/> */}
+            </main>
+          </div>
+        </Router>
       );
     }
 }
