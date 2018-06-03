d0f8da454e2a5d796dd7a8bb3825a81f1192d067
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 13:40:15 -0700

    setup the api call to handle paging

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -1,21 +1,3 @@
-// const BASE_URL = 'https://www.googleapis.com/books/v1';
-// const VOLUMES_URL = `${BASE_URL}/volumes?`;
-// const SORT_QUERY = 'orderBy=relevance';
-
-// const throwJson = json => { throw json; };
-// const get = url => fetch(url)
-//   .then(r => r.ok ? r.json() : r.json().then(throwJson));
-
-// export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 10 }) {
-//   const bookIndex = (page * pageSize) - pageSize;
-//   const search = `&q=${topic}&sources=${sources.join()}`;
-//   const maxResults = `&maxResults=${pageSize}`;
-//   const sort = `&${SORT_QUERY}`;
-//   const startIndex = `&startIndex=${bookIndex}`;
-
-//   return get(`${VOLUMES_URL}${search}${maxResults}${sort}${startIndex}`);
-// }
-
 const BASE_URL = 'https://www.googleapis.com/books/v1';
 
 const get = url => fetch(url)
@@ -27,8 +9,9 @@ export function checkResponseData(response) {
   return response;
 }
 
-export function search(term) {
-  const url = `${BASE_URL}/volumes?q=${term}`;
+export function search(term, page, perPage) {
+  const bookIndex = (page * perPage) - perPage;
+  const url = `${BASE_URL}/volumes?q=${term}&maxResults=${perPage}&startIndex=${bookIndex}`;
   return get(url);
 }
 
