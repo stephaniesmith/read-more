c82896e53c7e7c273d8c9ee3b694960bf27f2f08
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 13:24:11 -0700

    removed pagiong from the books component

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
-import Paging from './Paging';
 
 export default class Books extends Component {
 
   static propTypes = {
     books: PropTypes.array,
-    totalBooks: PropTypes.number
   };
 
   render() {
-    const { books, totalBooks } = this.props;
+    const { books } = this.props;
 
     return (
       <div>
-        <Paging key={totalBooks} {...totalBooks}/>
         <ul>
           {books.map(book => (
             <Book key={book.id} {...book}/>
