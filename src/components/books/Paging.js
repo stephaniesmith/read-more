1dbccc795fd3527819609b311fc9575b47d3b9f5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 13:24:41 -0700

    getting a simple state of totalBooks showing in the paging component

--- src/components/books/Paging.js
+++ src/components/books/Paging.js
@@ -13,17 +13,19 @@ export default class Books extends Component {
   }
 
   render() {
-    const { totalBooks, page, perPage } = this.props;
+    const { totalBooks } = this.props;
+    // const { totalBooks, page, perPage } = this.props;
 
     if(!totalBooks) return <div>No results found</div>;
+    if(totalBooks) return <div>{totalBooks}</div>;
 
-    const totalPages = Math.ceil(totalBooks / perPage);
-    return (
-      <div>
-        <p>Page {page} of {totalPages}</p>
-        <button onClick={() => this.handlePage(-1)} disabled={page === 1}>&lt; Prev</button>
-        <button onClick={() => this.handlePage(+1)} disabled={page === totalPages}>Next &gt;</button>
-      </div>
-    );
+    // const totalPages = Math.ceil(totalBooks / perPage);
+    // return (
+    //   <div>
+    //     <p>Page {page} of {totalPages}</p>
+    //     <button onClick={() => this.handlePage(-1)} disabled={page === 1}>&lt; Prev</button>
+    //     <button onClick={() => this.handlePage(+1)} disabled={page === totalPages}>Next &gt;</button>
+    //   </div>
+    // );
   }
 }
