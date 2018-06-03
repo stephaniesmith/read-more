de3b8276124cef9bc696e8ee34a05317393500b1
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 14:54:49 -0700

    set up the incrementing of pages in the Paging component

--- src/components/books/Paging.js
+++ src/components/books/Paging.js
@@ -6,19 +6,38 @@ export default class Books extends Component {
   static propTypes = {
     totalBooks: PropTypes.number,
     page: PropTypes.number,
-    perPage: PropTypes.number
+    perPage: PropTypes.number,
+    onPaging: PropTypes.func.isRequired
   };
 
+  state = {
+    current: this.props.page || 1,
+  };
+
+  componentWillReceiveProps({ page }) {
+    if(page !== this.state.current) {
+      this.setState({ current: page || 1 });
+    }
+  }
+
   handlePage(increment) {
-    const { page, onPage } = this.props;
-    onPage({ page: page + increment });
+    event.preventDefault();
+    const { page } = this.props;
+    this.setState({ current: page + increment }, () => {
+      this.callPage();
+    });
+  }
+
+  callPage() {
+    const { current } = this.state;
+    if(!current) return;
+    this.props.onPaging(current);
   }
 
   render() {
     const { totalBooks, page, perPage } = this.props;
 
     if(!totalBooks) return <div>No results found</div>;
-    if(totalBooks) return <div>{totalBooks}</div>;
 
     const totalPages = Math.ceil(totalBooks / perPage);
     return (
