a4578d3e2011ab84117acfe4c726adb51b9ae77c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 11:02:15 -0700

    created SearchForm

---
+++ src/components/search/SearchForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  
+  state = {
+    search: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ search: target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSearch(this.state);
+  };
+  
+  render() {
+    const { search } = this.state;
+
+    return (
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input placeholder="Search" value={search} onChange={this.handleChange}/>
+        <button>Search</button>
+      </form>
+    );
+  }
+}
