c2edbf9eb7be2fdb3636e44f95ebe14b515f656c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 12:18:23 -0700

    added props and state to book detail

--- src/components/books/BookDetail.js
+++ src/components/books/BookDetail.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types';
+import { getBook } from '../../services/booksApi';
+ 
 export default class BookDetail extends Component {
 
-  // static propTypes = {
-  //   id: PropTypes.string,
-  //   volumeInfo: PropTypes.object
-  // };
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    history: PropTypes.object
+  };
+
+  state = {
+    book: null
+  };
+
+  componentDidMount() {
+    getBook(this.props.id)
+      .then(book => this.setState({ book }));
+  }
     
   render() {
-    // const { id, volumeInfo } = this.props;
-    // const { title, imageLinks, authors, description } = volumeInfo;
+    const { volumeInfo } = this.props.book;
+    const { title, imageLinks, authors, description } = volumeInfo;
 
     return (
       <div>
         <p>Book Detail!!!</p>
-        {/* {title && <h3>Title: {title}</h3>}
+        {title && <h3>Title: {title}</h3>}
         {imageLinks.smallThumbnail && <img src={imageLinks.smallThumbnail}/>}
         {authors && <p>Author: {authors[0]}</p>}
-        {description && <p>{description}</p>} */}
+        {description && <p>{description}</p>}
       </div>
     );
   }
