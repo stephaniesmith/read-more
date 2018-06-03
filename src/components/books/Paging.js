4535783cd1e7d169ff06fe59b4357190b76af905
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 13:40:38 -0700

    setting up the paging component to control the paging

--- src/components/books/Paging.js
+++ src/components/books/Paging.js
@@ -5,6 +5,8 @@ export default class Books extends Component {
 
   static propTypes = {
     totalBooks: PropTypes.number,
+    page: PropTypes.number,
+    perPage: PropTypes.number
   };
 
   handlePage(increment) {
@@ -13,19 +15,18 @@ export default class Books extends Component {
   }
 
   render() {
-    const { totalBooks } = this.props;
-    // const { totalBooks, page, perPage } = this.props;
+    const { totalBooks, page, perPage } = this.props;
 
     if(!totalBooks) return <div>No results found</div>;
     if(totalBooks) return <div>{totalBooks}</div>;
 
-    // const totalPages = Math.ceil(totalBooks / perPage);
-    // return (
-    //   <div>
-    //     <p>Page {page} of {totalPages}</p>
-    //     <button onClick={() => this.handlePage(-1)} disabled={page === 1}>&lt; Prev</button>
-    //     <button onClick={() => this.handlePage(+1)} disabled={page === totalPages}>Next &gt;</button>
-    //   </div>
-    // );
+    const totalPages = Math.ceil(totalBooks / perPage);
+    return (
+      <div>
+        <p>Page {page} of {totalPages}</p>
+        <button onClick={() => this.handlePage(-1)} disabled={page === 1}>&lt; Prev</button>
+        <button onClick={() => this.handlePage(+1)} disabled={page === totalPages}>Next &gt;</button>
+      </div>
+    );
   }
 }
