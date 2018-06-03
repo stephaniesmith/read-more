a8fe82fdd5e1fe2d31541998492448e9ee6df330
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 11:08:34 -0700

    trying to get a call from the books api

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -1,17 +1,34 @@
+// const BASE_URL = 'https://www.googleapis.com/books/v1';
+// const VOLUMES_URL = `${BASE_URL}/volumes?`;
+// const SORT_QUERY = 'orderBy=relevance';
+
+// const throwJson = json => { throw json; };
+// const get = url => fetch(url)
+//   .then(r => r.ok ? r.json() : r.json().then(throwJson));
+
+// export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 10 }) {
+//   const bookIndex = (page * pageSize) - pageSize;
+//   const search = `&q=${topic}&sources=${sources.join()}`;
+//   const maxResults = `&maxResults=${pageSize}`;
+//   const sort = `&${SORT_QUERY}`;
+//   const startIndex = `&startIndex=${bookIndex}`;
+
+//   return get(`${VOLUMES_URL}${search}${maxResults}${sort}${startIndex}`);
+// }
+
 const BASE_URL = 'https://www.googleapis.com/books/v1';
 const VOLUMES_URL = `${BASE_URL}/volumes?`;
-const SORT_QUERY = 'orderBy=relevance';
 
-const throwJson = json => { throw json; };
 const get = url => fetch(url)
-  .then(r => r.ok ? r.json() : r.json().then(throwJson));
+  .then(response => response.json())
+  .then(checkResponseData);
 
-export function search({ topic, sources = ['bad'] }, { page = 1, pageSize = 10 }) {
-  const bookIndex = (page * pageSize) - pageSize;
-  const search = `&q=${topic}&sources=${sources.join()}`;
-  const maxResults = `&maxResults=${pageSize}`;
-  const sort = `&${SORT_QUERY}`;
-  const startIndex = `&startIndex=${bookIndex}`;
+export function checkResponseData(response) {
+  if(response.Response === 'False') throw response.Error;
+  return response;
+}
 
-  return get(`${VOLUMES_URL}${search}${maxResults}${sort}${startIndex}`);
+export function search(term) {
+  const url = `${VOLUMES_URL}&q=${term}`;
+  return get(url);
 }
