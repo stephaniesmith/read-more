cb9f1dca475193b7579dab4d789e78f5ee1e1167
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-03 17:18:23 -0700

    removed commented out code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import { search } from '../../services/booksApi';
 import Header from './Header';
 import Home from './Home';
 import Search from '../search/Search';
 import BookDetail from '../books/BookDetail';
-// import Books from '../Books';
-// import Paging from '../Paging';
 
 export default class App extends Component {
-    
-  // state = {
-  //   topic: '',
-  //   loading: false,
-  //   error: null,
-  //   page: 1,
-  //   perPage: 10,
-  //   totalItems: 0,
-  //   items: []
-  // };
-
-  // searchBooks = () => {
-  //   const { topic, page, perPage } = this.state;
-
-  //   this.setState({ loading: true });
-
-  //   search({ topic }, { page, perPage })
-  //     .then(({ items, totalItems }) => {
-  //       this.setState({ items, totalItems, error: null });
-  //     }, error => {
-  //       this.setState({ error });
-  //     })
-  //     .then(() => this.setState({ loading: false }));
-
-  // };
-
-  // handleSearch = ({ search }) => {
-  //   this.setState({ topic: search, page: 1 }, this.searchBooks);
-  // };
-
-  // handlePage = ({ page }) => {
-  //   this.setState({ page }, this.searchBooks);
-  // };
 
   render() {
-    // const { topic, loading, error, page, perPage, totalItems, items } = this.state;
 
     return (
       <Router>
         <div>
           <Header/>
-          {/* <Search onSearch={this.handleSearch}/> */}
           <main>
             <Switch>
               <Route exact path="/" component={Home}/>
@@ -63,14 +25,7 @@ export default class App extends Component {
                 {loading && <div>Loading...</div>}
                 {error && <div>Error: {error.message}</div>}
                 <p>Results for: {topic}</p>
-              </section>
-              <Paging
-                totalItems={totalItems}
-                page={page}
-                perPage={perPage}
-                onPage={this.handlePage}
-              />
-              <Books items={items}/> */}
+              </section>*/}
           </main>
         </div>
       </Router>
