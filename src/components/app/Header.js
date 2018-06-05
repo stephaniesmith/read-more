ca4ed17c1d40ba16426980523de4e1408c4a47a3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-05 08:57:24 -0700

    added logo

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from './booklogo.png';
 
 export default class Header extends Component {
     
@@ -7,10 +8,9 @@ export default class Header extends Component {
 
     return (
       <div>
-        <h1>Find a Book</h1>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/"><img src={logo} width="100"/></Link></li>
             <li><Link to="/search" >Search</Link></li>
           </ul>
         </nav>
